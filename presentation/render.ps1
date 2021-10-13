2adb15be381b24dfc6b43697c4d587f0b65ec73d
Chéyo Jiménez <masters3d@users.noreply.github.com>
2021-10-12 18:38:32 -0700

    render inprove single

--- presentation/render.ps1
+++ presentation/render.ps1
@@ -1,12 +1,28 @@
 
 
  param (
-    [bool]$clearCache = $false
+    [bool]$clearCache = $false,
+    [string]$single = "none"
  )
+
 
 # Get-Location only gets the current workng path. See https://www.autoitconsulting.com/site/scripting/get-current-script-directory-powershell-vbscript-batch/
 $location = Split-Path $script:MyInvocation.MyCommand.Path  
 $renderLocation = "$location/render"
+
+function executeWithFilename {
+    param (
+        [string]$name
+        )
+        Write-Output "creating powerpoint for $name"
+        pandoc $location/$name.md -o $renderLocation/$name.pptx
+}
+
+if ($single -ne "none") {
+    executeWithFilename $single
+    exit(0);
+}
+
 
 if ($clearCache) {
     foreach ($each in Get-ChildItem -Path "$renderLocation" ) {
@@ -29,8 +45,7 @@ foreach ($each in Get-ChildItem -Path "$location" ) {
     if ("$each".EndsWith($filetype))
     {
         $name = "$each".Substring(0, "$each".Length - $filetype.Length)
-        Write-Output "creating powerpoint for $name"
-        pandoc $location/$name.md -o $renderLocation/$name.pptx
+        executeWithFilename $name
     }
  
 }
