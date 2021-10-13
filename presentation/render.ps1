d51ceb1c476d009543220a20ca79b1df0ca68b04
Chéyo Jiménez <masters3d@users.noreply.github.com>
2021-10-12 18:04:49 -0700

    adding powerpoint rendering

--- presentation/render.ps1
+++ presentation/render.ps1
@@ -1,5 +1,25 @@
 
-$location = Get-Location
+
+ param (
+    [bool]$clearCache = $false
+ )
+
+# Get-Location only gets the current workng path. See https://www.autoitconsulting.com/site/scripting/get-current-script-directory-powershell-vbscript-batch/
+$location = Split-Path $script:MyInvocation.MyCommand.Path  
+$renderLocation = "$location/render"
+
+if ($clearCache) {
+    foreach ($each in Get-ChildItem -Path "$renderLocation" ) {
+        $filetype = ".pptx"
+        if ("$each".EndsWith($filetype))
+        {
+            Write-Output "deleting $each"
+            Remove-Item $renderLocation/$each
+        }
+     
+    }
+}
+
 foreach ($each in Get-ChildItem -Path "$location" ) {
     if ("$each" -eq "README.md") 
     {
@@ -9,8 +29,8 @@ foreach ($each in Get-ChildItem -Path "$location" ) {
     if ("$each".EndsWith($filetype))
     {
         $name = "$each".Substring(0, "$each".Length - $filetype.Length)
-        Write-Output "$name"
-        pandoc $location/$name.md -o $location/render/$name.pptx
+        Write-Output "creating powerpoint for $name"
+        pandoc $location/$name.md -o $renderLocation/$name.pptx
     }
  
 }
