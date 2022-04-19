88a3ea4629e3e9da3bdedb09435ba5c5ee1deff6
Chéyo Jiménez <masters3d@users.noreply.github.com>
2022-04-19 14:50:11 -0700

    Update doc2md.ps1

--- presentation/doc2md.ps1
+++ presentation/doc2md.ps1
@@ -9,8 +9,12 @@
 # $location = Split-Path $script:MyInvocation.MyCommand.Path  
 $location = Get-Location
 $renderLocation = $location
+# The media folder here is matches the name of an already created file. pandoc creates a /media/ folder but we are going to add the name of the file as subfolder
+# final path will be something like  "$location/media/$name/media" where the last media is create by pandoc
+$renderLocationForMediaRoot = "$location/media/" 
 $filetype_source = "docx"
 $filetype_target = "md"
+$filename_temp_prefix = "___temp___"
 
 Write-Output "location $location"
 
@@ -20,14 +24,21 @@ function executeWithFilename {
         [string]$name
         )
         $sourceFile = "$location/$name.$filetype_source"
+        $sourceFileTemp = "$location/$filename_temp_prefix$name.$filetype_source"
         $outputFile = "$renderLocation/${name}_$filetype_source.$filetype_target"
+        # the naming schema is not unique image1.png| image2.png so we need to add the name of the file to distiwish
+        $renderLocationForMediaForFile = "$renderLocationForMediaRoot/$name"
+
+        # We have to make a copy since pandoc is not able to open the original file
+        # Erro from pandoc openBinaryFile: permission denied (Permission denied)
+        copy-item -path $sourceFile -destination $sourceFileTemp
 
         Write-Output "creating $filetype_target for $name"
 
         Write-Output "sourceFile $sourceFile"
         Write-Output "outputFile $outputFile" 
 
-        pandoc $sourceFile -t gfm -o $outputFile
+        pandoc $sourceFileTemp --extract-media $renderLocationForMediaForFile -t gfm -o $outputFile
     }
 
 if ($single -ne "none") {
@@ -38,6 +49,11 @@ if ($single -ne "none") {
 foreach ($each in Get-ChildItem -Path "$location" ) {
     if ("$each".EndsWith($filetype_source))
     {
+        if ("$each".StartsWith($filename_temp_prefix))
+        {
+            continue
+        }
+
         $name = "$each".Substring(0, "$each".Length - $filetype_source.Length - 1)
         executeWithFilename $name
     }
