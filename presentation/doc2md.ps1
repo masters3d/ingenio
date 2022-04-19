e20f361c8ab67e41dd286cedde1a88f905d853b0
Chéyo Jiménez <masters3d@users.noreply.github.com>
2022-04-19 15:23:19 -0700

    Update doc2md.ps1

--- presentation/doc2md.ps1
+++ presentation/doc2md.ps1
@@ -9,9 +9,7 @@
 # $location = Split-Path $script:MyInvocation.MyCommand.Path  
 $location = Get-Location
 $renderLocation = $location
-# The media folder here is matches the name of an already created file. pandoc creates a /media/ folder but we are going to add the name of the file as subfolder
-# final path will be something like  "$location/media/$name/media" where the last media is create by pandoc
-$renderLocationForMediaRoot = "$location/media/" 
+
 $filetype_source = "docx"
 $filetype_target = "md"
 $filename_temp_prefix = "___temp___"
@@ -26,6 +24,12 @@ function executeWithFilename {
         $sourceFile = "$location/$name.$filetype_source"
         $sourceFileTemp = "$location/$filename_temp_prefix$name.$filetype_source"
         $outputFile = "$renderLocation/${name}_$filetype_source.$filetype_target"
+
+        # The media folder here is matches the name of an already created file. pandoc creates a /media/ folder but we are going to add the name of the file as subfolder
+        # final path will be something like  "$location/media/$name/media" where the last media is create by pandoch
+        # we want the paths to be relative so we are going to use . for current folder
+        $renderLocationForMediaRoot = "./media" 
+
         # the naming schema is not unique image1.png| image2.png so we need to add the name of the file to distiwish
         $renderLocationForMediaForFile = "$renderLocationForMediaRoot/$name"
 
